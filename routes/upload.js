6adf4f3cfa6a7f51c3b44d1cddda9b176ab50431
evstarostin89 <evstarostin89@yandex-team.ru>
2019-03-24 23:31:39 +0300

    +

---
+++ routes/upload.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path')
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+router.post('/', upload.single('file'), function (req, res, next) {
+  if (req.file) {
+    console.log('file uploaded');
+  }
+
+  res.redirect('/');
+})
+
+module.exports = router;
