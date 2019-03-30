ee2bbe958e13c2df8982cf06328765bda6cb714f
evstarostin89 <evstarostin89@yandex-team.ru>
2019-03-30 16:50:43 +0300

    Изменены стили

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,12 @@
+var fieldset = document.querySelector('.upload__fieldset');
+var file = document.querySelector('.upload__file');
+
 function log(html) {
     document.getElementById('log').innerHTML = html;
+}
+
+function drawGradient(percent) { 
+    fieldset.style.background = 'linear-gradient(to right, green, lightgreen ' + percent + '%, #fff ' + percent + '%)';
 }
 
 function upload(formData) {
@@ -8,21 +15,27 @@ function upload(formData) {
     // обработчик для закачки
     xhr.upload.onprogress = function(event) {
         var progress = Math.round(event.loaded * 100 / event.total);
-        log('Progress: ' + progress + '%');
+        log('Завершено: ' + progress + '%');
+        drawGradient(progress);
     }
   
     // обработчики успеха и ошибки
     // если status == 200, то это успех, иначе ошибка
     xhr.onload = xhr.onerror = function() {
         if (this.status == 200) {
-            log("Success");
+            log('Выполнено');
         } else {
-            log("Error " + this.status);
+            log('Ошибка ' + this.status);
         }
     };
   
-    xhr.open("POST", "upload", true);
+    xhr.open('POST', 'upload', true);
     xhr.send(formData);
+
+    file.addEventListener('click', function(e) {
+        log('');
+        fieldset.style.background = '';
+    });
 }
 
 document.forms.upload.onsubmit = function(e) {
